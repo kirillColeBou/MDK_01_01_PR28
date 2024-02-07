a4549a4ce1182eff50fc9fa642a40ba31fa3b0ae
kirillColeBou <150902802+kirillColeBou@users.noreply.github.com>
2024-02-07 21:10:11 +0500

    Реализовано окно MainWindow. Тепляков

--- ПР28/pcClub_Тепляков/pcClub_Тепляков/MainWindow.xaml.cs
+++ ПР28/pcClub_Тепляков/pcClub_Тепляков/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static pcClub_Тепляков.MainWindow;
 
 namespace pcClub_Тепляков
 {
@@ -20,9 +21,31 @@ namespace pcClub_Тепляков
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            OpenPages(pages.login);
+        }
+
+        public enum pages
+        {
+            login, club, addclub, rent, addrent
+        }
+
+        public void OpenPages(pages _pages)
+        {
+            if (_pages == pages.login)
+                frame.Navigate(new Pages.Login());
+            if (_pages == pages.club)
+                frame.Navigate(new Pages.Club());
+            if (_pages == pages.addclub)
+                frame.Navigate(new Pages.AddClub());
+            if (_pages == pages.rent)
+                frame.Navigate(new Pages.Rent());
+            if (_pages == pages.addrent)
+                frame.Navigate(new Pages.AddRent());
         }
     }
 }
